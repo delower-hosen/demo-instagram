54b775faddbf0ed7ac0ed72473354a8cc33855cc
Delower <delowerhosencuet13@gmail.com>
2019-08-22 01:20:59 +0600

    login updated

--- src/app/app-login/app-login.module.ts
+++ src/app/app-login/app-login.module.ts
@@ -3,12 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { AppLoginRoutingModule } from './app-login-routing.module';
 import { AppLoginDefaultComponent } from './components/app-login-default/app-login-default.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppLoginComponent } from './components/app-login/app-login.component';
+import { AppRegistrationComponent } from './components/app-registration/app-registration.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppLoginDefaultComponent],
+  declarations: [AppLoginDefaultComponent, AppLoginComponent, AppRegistrationComponent],
   imports: [
     CommonModule,
-    AppLoginRoutingModule
+    AppLoginRoutingModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatCardModule
   ]
 })
 export class AppLoginModule { }
--- src/app/app-login/components/app-login-default/app-login-default.component.ts
+++ src/app/app-login/components/app-login-default/app-login-default.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppLoginDefaultComponent implements OnInit {
 
+  public coverPhoto: string;
+  public selectedStettingMenu: string = 'login';
+  public selectedMenuIndex = 0;
   constructor() { }
 
   ngOnInit() {
+    this.coverPhoto = require('./../../../../assets/porfile/cover.png');
   }
 
+  updateSelectedMenu(event) {
+    // if (event.index == 0) this.selectedStettingMenu = 'login';
+    // else if (event.index == 1) this.selectedStettingMenu = 'registration';
+    if(event.index == 0) this.selectedMenuIndex = 0;
+    else if(event.index == 1) this.selectedMenuIndex = 1;
+  }
+
+  updateSelectedIndex(index: number){
+    this.selectedMenuIndex = index;
+  }
 }
